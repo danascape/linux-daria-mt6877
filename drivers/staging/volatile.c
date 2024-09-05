0a0a9a531e919280e74ef89730e911f2d7dfe839
Saalim Quadri <danascape@gmail.com>
2024-09-05 09:50:51 +0530

    staging: Map physical offset of file

--- drivers/staging/volatile.c
+++ drivers/staging/volatile.c
@@ -4,11 +4,13 @@
 #include<linux/init.h>
 #include<linux/kernel.h>
 #include<linux/fs.h>
+#include<linux/fuse.h>
 #include<linux/err.h>
 #include<linux/errno.h>
 #include<linux/file.h>
 #include<linux/fcntl.h>
 #include<linux/uaccess.h>
+#include <linux/buffer_head.h>
 
 char * input_path = 0;
 static struct file * fp = NULL;
@@ -21,8 +23,12 @@ static int openFile(char *path)
 	int err = 0;
 	const char *name;
 	unsigned long ino = 0;
+	unsigned long bn = 0;
+	sector_t block = 0;
 
 	struct inode *inode;
+	struct buffer_head *bh;
+	struct super_block *sb;
 
 	pr_info("volatile: openFile reached with %s\n", path);
 
@@ -42,6 +48,28 @@ static int openFile(char *path)
 
 	pr_info("volatile: File opened: %lld bytes\n", i_size_read(inode));
 	pr_info("volatile: File opened: %lu inode\n", inode->i_ino);
+
+	sb = inode->i_sb;
+    block = bmap(inode, 0);
+    if (!block) {
+        pr_info("volatile: Unable to map physical block\n");
+        filp_close(fp, NULL);
+        // return -EINVAL;
+    }
+
+    pr_info("volatile: Physical Block: %llu\n", (unsigned long long)block);
+
+	// block = inode->i_mapping->host->i_ino / (4096 / sb->s_blocksize);
+    bh = sb_bread(sb, block);
+    if (!bh) {
+        printk(KERN_ERR "Cannot read the block\n");
+        filp_close(fp, NULL);
+        return -EIO;
+    }
+
+    pr_info("volatile: Physical address: %llu\n", (unsigned long long)bh->b_blocknr * 4096);
+
+	brelse(bh);
 
 	filp_close(fp, NULL);
 	return 0;
